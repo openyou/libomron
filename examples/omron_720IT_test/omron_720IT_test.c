10f0c7c89526d8a4287ae166480b22b7e8820b37
Kyle Machulis <kyle@nonpolynomial.com>
2010-04-11 17:56:21 -0700

    Lots of temp changes for libusb-1.0 testing

--- examples/omron_720IT_test/omron_720IT_test.c
+++ examples/omron_720IT_test/omron_720IT_test.c
@@ -1,10 +1,10 @@
-#include "omron.h"
+#include "libomron/omron.h"
 #include <stdio.h>
 #include <stdlib.h>		/* atoi */
 
 int main(int argc, char** argv)
 {
-	omron_device test;
+	omron_device* test;
 	int ret;
 	int i;
 	int data_count;
@@ -20,8 +20,10 @@ int main(int argc, char** argv)
 	//hid_set_debug_stream(stderr);
 	//hid_set_usb_debug(0);
 	//#endif USE_LIBHID
+
+	test = omron_create();
 	
-	ret = omron_get_count(OMRON_790IT_VID, OMRON_790IT_PID);
+	ret = omron_get_count(test, OMRON_790IT_VID, OMRON_790IT_PID);
 
 	if(!ret)
 	{
@@ -30,14 +32,15 @@ int main(int argc, char** argv)
 	}
 	printf("Found %d omron 790ITs\n", ret);
 
-	ret = omron_open(&test, OMRON_790IT_VID, OMRON_790IT_PID, 0);
+	ret = omron_open(test, OMRON_790IT_VID, OMRON_790IT_PID, 0);
 	if(ret < 0)
 	{
 		printf("Cannot open omron 790IT!\n");
 		return 1;
 	}
+	printf("Opened omron 720IT\n", ret);
 
-	ret = omron_get_device_version(&test, str);
+	ret = omron_get_device_version(test, str);
 	if(ret < 0)
 	{
 		printf("Cannot get device version!\n");
@@ -47,18 +50,18 @@ int main(int argc, char** argv)
 		printf("Device serial: %s\n", str);
 	}
 
-	omron_pd_profile_info p = omron_get_pd_profile(&test);
+	omron_pd_profile_info p = omron_get_pd_profile(test);
 	printf("Weight: %d pounds\n", p.weight);
 	printf("Stride: %d inches\n", p.stride); 
-	omron_pd_count_info c = omron_get_pd_data_count(&test);
+	omron_pd_count_info c = omron_get_pd_data_count(test);
 	printf("Daily Reading Blocks: %d\n", c.daily_count);
 	printf("Hourly Reading Blocks: %d\n", c.hourly_count);
 
 	for(i = 0; i < c.daily_count - 1; ++i)
 	{
-		omron_pd_daily_data d = omron_get_pd_daily_data(&test, i + 1);
+		omron_pd_daily_data d = omron_get_pd_daily_data(test, i + 1);
 		printf("Daily Steps: %d\n", d.total_steps);
-		omron_pd_hourly_data* h = omron_get_pd_hourly_data(&test, i + 1);
+		omron_pd_hourly_data* h = omron_get_pd_hourly_data(test, i + 1);
 		int j;
 		for(j = 0; j < 24; ++j)
 		{
@@ -66,7 +69,7 @@ int main(int argc, char** argv)
 		}
 	}
 
-	ret = omron_close(&test);
+	ret = omron_close(test);
 	if(ret < 0)
 	{
 		printf("Cannot close omron 790IT!\n");
